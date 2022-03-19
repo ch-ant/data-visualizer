bbf72318cfb4714403f72e14e47136a0868b6123
ch-ant <ant.chr@outlook.com>
2022-03-19 18:55:41 +0200

    Update config.ts
    Change server localhost port to 8080

--- src/config/config.ts
+++ src/config/config.ts
@@ -15,7 +15,7 @@ const MYSQL = {
 };
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 3131;
+const SERVER_PORT = process.env.SERVER_PORT || 8080;
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
