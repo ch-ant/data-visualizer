6dc4163cfc4e92b70e45fa869f3fd805f4570c3c
ch-ant <ant.chr@outlook.com>
2022-03-19 20:44:04 +0200

    Add ETL related files
    - Move schema.sql to schema directory
    - Add etl directory
    - Add transform.py data transformer script
    - Add load.sql data loader script
    - Add extracted data files in the respective directory
    - Add transformed data files in the respective directory

---
+++ db/etl/load.sql
@@ -0,0 +1,95 @@
+/* 
+	This loader script is part of the data ETL process.
+    It takes the transformed data, country metadata and
+    indicator metadata files as input and loads them into
+    the database using the LOAD DATA INFILE command.
+    The full path of each data (or metadata) file should
+    be specified in the first line of each LOAD DATA INFILE
+    command.
+
+	How to run: Open the script in MySQL Workbench and 
+	click execute.
+*/
+
+
+USE data_visualizer;
+SET GLOBAL local_infile = TRUE;
+
+
+CREATE TABLE temp_measurements (
+	country_code VARCHAR(32) NOT NULL,
+    indicator_code VARCHAR(32) NOT NULL,
+    `1960` DOUBLE, `1961` DOUBLE, `1962` DOUBLE, `1963` DOUBLE, `1964` DOUBLE,
+	`1965` DOUBLE, `1966` DOUBLE, `1967` DOUBLE, `1968` DOUBLE, `1969` DOUBLE,
+	`1970` DOUBLE, `1971` DOUBLE, `1972` DOUBLE, `1973` DOUBLE, `1974` DOUBLE,
+	`1975` DOUBLE, `1976` DOUBLE, `1977` DOUBLE, `1978` DOUBLE, `1979` DOUBLE, 
+	`1980` DOUBLE, `1981` DOUBLE, `1982` DOUBLE, `1983` DOUBLE, `1984` DOUBLE,
+	`1985` DOUBLE, `1986` DOUBLE, `1987` DOUBLE, `1988` DOUBLE, `1989` DOUBLE, 
+	`1990` DOUBLE, `1991` DOUBLE, `1992` DOUBLE, `1993` DOUBLE, `1994` DOUBLE,
+	`1995` DOUBLE, `1996` DOUBLE, `1997` DOUBLE, `1998` DOUBLE, `1999` DOUBLE, 
+	`2000` DOUBLE, `2001` DOUBLE, `2002` DOUBLE, `2003` DOUBLE, `2004` DOUBLE,
+	`2005` DOUBLE, `2006` DOUBLE, `2007` DOUBLE, `2008` DOUBLE, `2009` DOUBLE, 
+	`2010` DOUBLE, `2011` DOUBLE, `2012` DOUBLE, `2013` DOUBLE, `2014` DOUBLE,
+	`2015` DOUBLE, `2016` DOUBLE, `2017` DOUBLE, `2018` DOUBLE, `2019` DOUBLE, 
+	`2020` DOUBLE
+);
+
+
+-- Specify csv country metadata file path here
+LOAD DATA LOCAL INFILE 'E:/node-projects/data-visualizer/db/etl/transformed_data/transformed_country_metadata.csv'
+INTO TABLE countries
+CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(name, code, region, income_group, special_notes);
+
+
+-- Specify csv indicator metadata file path here
+LOAD DATA LOCAL INFILE 'E:/node-projects/data-visualizer/db/etl/transformed_data/transformed_indicator_metadata.csv'
+INTO TABLE indicators
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(name, code, source_note, source_organization);
+
+
+-- Specify csv data file path here
+LOAD DATA LOCAL INFILE 'E:/node-projects/data-visualizer/db/etl/transformed_data/transformed_data.csv'
+INTO TABLE temp_measurements
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(country_code, indicator_code,
+`1960`, `1961`, `1962`, `1963`, `1964`, `1965`, `1966`, `1967`, `1968`, `1969`,
+`1970`, `1971`, `1972`, `1973`, `1974`, `1975`, `1976`, `1977`, `1978`, `1979`, 
+`1980`, `1981`, `1982`, `1983`, `1984`, `1985`, `1986`, `1987`, `1988`, `1989`, 
+`1990`, `1991`, `1992`, `1993`, `1994`, `1995`, `1996`, `1997`, `1998`, `1999`, 
+`2000`, `2001`, `2002`, `2003`, `2004`, `2005`, `2006`, `2007`, `2008`, `2009`, 
+`2010`, `2011`, `2012`, `2013`, `2014`, `2015`, `2016`, `2017`, `2018`, `2019`, 
+`2020`);
+
+
+INSERT INTO measurements 
+(country_id, indicator_id,
+`1960`, `1961`, `1962`, `1963`, `1964`, `1965`, `1966`, `1967`, `1968`, `1969`,
+`1970`, `1971`, `1972`, `1973`, `1974`, `1975`, `1976`, `1977`, `1978`, `1979`, 
+`1980`, `1981`, `1982`, `1983`, `1984`, `1985`, `1986`, `1987`, `1988`, `1989`, 
+`1990`, `1991`, `1992`, `1993`, `1994`, `1995`, `1996`, `1997`, `1998`, `1999`, 
+`2000`, `2001`, `2002`, `2003`, `2004`, `2005`, `2006`, `2007`, `2008`, `2009`, 
+`2010`, `2011`, `2012`, `2013`, `2014`, `2015`, `2016`, `2017`, `2018`, `2019`, 
+`2020`)
+SELECT DISTINCT countries.id, indicators.id, -- countries.code, indicators.code,
+`1960`, `1961`, `1962`, `1963`, `1964`, `1965`, `1966`, `1967`, `1968`, `1969`,
+`1970`, `1971`, `1972`, `1973`, `1974`, `1975`, `1976`, `1977`, `1978`, `1979`, 
+`1980`, `1981`, `1982`, `1983`, `1984`, `1985`, `1986`, `1987`, `1988`, `1989`, 
+`1990`, `1991`, `1992`, `1993`, `1994`, `1995`, `1996`, `1997`, `1998`, `1999`, 
+`2000`, `2001`, `2002`, `2003`, `2004`, `2005`, `2006`, `2007`, `2008`, `2009`, 
+`2010`, `2011`, `2012`, `2013`, `2014`, `2015`, `2016`, `2017`, `2018`, `2019`, 
+`2020`
+FROM countries, indicators, temp_measurements
+WHERE countries.code = temp_measurements.country_code 
+AND indicators.code = temp_measurements.indicator_code;
+
+
+DROP TABLE temp_measurements;
---
+++ db/etl/transform.py
@@ -0,0 +1,222 @@
+#
+#   This transformer script is part of the data ETL process.
+#   It takes the extracted data, country metadata and indicator
+#   metadata files as input and transforms them into three csv 
+#   files: data, country metadata and indicator metadata 
+#   respectively. The transformed files contain the data (or metadata)
+#   in a format that is ready to be loaded into the database using 
+#   the load.sql script in the same directory. Input and output directories
+#   as well as names for the transformed data files can be specified below.
+#   The extracted data files must be placed in the input directory that 
+#   was specified.
+#
+#   Run command:    python transform.py
+#   Preferably execute in Linux environment   
+#
+
+
+from csv import reader
+from os import chdir
+from glob import glob
+
+
+# Specify input and output directories
+INPUT_DIR = "./extracted_data"
+OUTPUT_DIR = "../transformed_data"
+
+# Specify ouput file names for the transformed data
+OUTPUT_COUNTRY_META_FILE = 'transformed_country_metadata.csv'
+OUTPUT_INDICATOR_META_FILE = 'transformed_indicator_metadata.csv'
+OUTPUT_DATA_FILE = 'transformed_data.csv'
+
+
+class Measurement:
+    def __init__(self, countryCode, indicatorCode):
+        self.countryCode = countryCode
+        self.indicatorCode = indicatorCode
+        self.values = []
+
+    def toString(self):
+        delimiter = "\",\""
+        return "\"" + self.countryCode + delimiter + \
+                self.indicatorCode + delimiter + \
+                delimiter.join(self.values) + "\""
+
+
+class Country:
+    def __init__(self, name, code, region, incomeGroup, specialNotes):
+        self.name = name
+        self.code = code
+        self.region = region
+        self.incomeGroup = incomeGroup
+        self.specialNotes = specialNotes
+
+    def existsInList(self, countries):
+        for country in countries:
+            if self.name == country.name and self.code == country.code:
+                return True
+        return False
+
+    def toString(self):
+            delimiter = "\",\""
+            return "\"" + self.name + delimiter + \
+                    self.code + delimiter + \
+                    self.region + delimiter + \
+                    self.incomeGroup + delimiter + \
+                    self.specialNotes + "\""
+
+
+class Indicator:
+    def __init__(self, name, code, sourceNote, sourceOrganization):
+        self.name = name
+        self.code = code
+        self.sourceNote = sourceNote
+        self.sourceOrganization = sourceOrganization
+    
+    def existsInList(self, indicators):
+        for indicator in indicators:
+            if self.name == indicator.name and \
+                    self.code == indicator.code and \
+                    self.sourceNote == indicator.sourceNote and \
+                    self.sourceOrganization == indicator.sourceOrganization:
+                return True
+        return False
+
+    def toString(self):
+        delimiter = "\",\""
+        return "\"" + self.name + delimiter + \
+                self.code + delimiter + \
+                self.sourceNote + delimiter + \
+                self.sourceOrganization + "\""
+
+
+
+
+def findInputFiles():
+    chdir(INPUT_DIR)
+    return glob("*.csv")
+
+
+def transformInputFiles(inputFiles):
+    for file in inputFiles:
+        data = readFileToString(file)
+        if file.startswith("API"):
+            transformDataFile(data)
+        elif file.startswith('Metadata_Country'):
+            transformCountryMetadataFile(data)
+        elif file.startswith('Metadata_Indicator'):
+            transformMetadataIndicatorFile(data)
+
+
+def transformDataFile(data):
+    data = transformNullValues(data)
+    data = data.splitlines()
+    years = findYears(data)
+    data = readCsv(data, 5)
+    transformData(data, years)
+
+
+def transformCountryMetadataFile(data):
+    data = transformNullValues(data)
+    data = data.splitlines()
+    tableNameIndex, specialNotesIndex = findColumnNameIndexes(data[0])
+    data = readCsv(data, 1)
+    transformCountryMetadata(data, tableNameIndex, specialNotesIndex)
+    
+
+def transformMetadataIndicatorFile(data):
+    data = data.splitlines()
+    data = readCsv(data, 1)
+    transformIndicatorMetadata(data)
+
+
+def readFileToString(file):
+    file = open(file, "rt", encoding="utf8")
+    data = file.read()
+    file.close()
+    return data
+
+
+def transformNullValues(data):
+    return data.replace("\"\"", "\"\\N\"")
+
+
+def findYears(data):
+    header = data[4].replace('"', '')
+    years = header.split(',')[4:]
+    del years[-1]
+    return years
+
+
+def readCsv(data, startLine):
+    data = reader(data[startLine:], delimiter = ',')
+    return list(data)
+
+
+def transformData(data, years):
+    for line in data:
+        measurement = Measurement(line[1], line[3])
+        counter = 4
+        for year in years:
+            measurement.values.append(line[counter])
+            counter += 1
+        measurements.append(measurement)
+
+
+def transformCountryMetadata(data, tableNameIndex, specialNotesIndex):
+    for line in data:
+        country = Country(line[tableNameIndex], line[0], line[1], line[2], line[specialNotesIndex])
+        if not country.existsInList(countries):
+            countries.append(country)
+
+
+def transformIndicatorMetadata(data):
+    for line in data:
+        indicator = Indicator(line[1], line[0], line[2], line[3])
+        if not indicator.existsInList(indicators):
+            indicators.append(indicator)
+
+
+def findColumnNameIndexes(header):
+    header = header.replace('"', '').split(',')
+    for column in header:
+        if column == "TableName":
+            tableNameIndex = header.index(column)
+        elif column == "SpecialNotes":
+            specialNotesIndex = header.index(column)
+    return tableNameIndex, specialNotesIndex
+
+
+def createOutputFiles():
+    chdir(OUTPUT_DIR)
+    createOutputFile(countries, OUTPUT_COUNTRY_META_FILE)
+    createOutputFile(indicators, OUTPUT_INDICATOR_META_FILE)
+    createOutputFile(measurements, OUTPUT_DATA_FILE)
+
+
+def createOutputFile(list, outputFile):
+    buf = ''
+    for object in list:
+        buf += object.toString() + ",\n"
+    writeToFile(buf, outputFile, list)
+
+
+def writeToFile(buf, file, list):
+    msg = '>>> Transformed ' + str(len(list)) + ' lines in file:\t' + file
+    file = open(file, "w+", encoding="utf8")
+    file.write(buf)
+    file.close()
+    print(msg)
+
+
+
+
+if __name__ == '__main__':
+    measurements = []
+    countries = []
+    indicators = []
+    indicatorSources = []
+
+    inputFiles = findInputFiles()
+    transformInputFiles(inputFiles)
+    createOutputFiles()
