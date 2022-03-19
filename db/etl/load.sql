74cbb27d492b0931401f50c9cb914c7ac71f75aa
ch-ant <ant.chr@outlook.com>
2022-03-19 21:03:22 +0200

    Update load.sql
    Add latin1 as default charset to temp_measurements table to avoid collation conversion issues.

--- db/etl/load.sql
+++ db/etl/load.sql
@@ -32,7 +32,9 @@ CREATE TABLE temp_measurements (
 	`2010` DOUBLE, `2011` DOUBLE, `2012` DOUBLE, `2013` DOUBLE, `2014` DOUBLE,
 	`2015` DOUBLE, `2016` DOUBLE, `2017` DOUBLE, `2018` DOUBLE, `2019` DOUBLE, 
 	`2020` DOUBLE
-);
+)
+ENGINE = InnoDB 
+DEFAULT CHARSET = latin1;
 
 
 -- Specify csv country metadata file path here
@@ -79,7 +81,7 @@ INSERT INTO measurements
 `2000`, `2001`, `2002`, `2003`, `2004`, `2005`, `2006`, `2007`, `2008`, `2009`, 
 `2010`, `2011`, `2012`, `2013`, `2014`, `2015`, `2016`, `2017`, `2018`, `2019`, 
 `2020`)
-SELECT DISTINCT countries.id, indicators.id, -- countries.code, indicators.code,
+SELECT countries.id, indicators.id,
 `1960`, `1961`, `1962`, `1963`, `1964`, `1965`, `1966`, `1967`, `1968`, `1969`,
 `1970`, `1971`, `1972`, `1973`, `1974`, `1975`, `1976`, `1977`, `1978`, `1979`, 
 `1980`, `1981`, `1982`, `1983`, `1984`, `1985`, `1986`, `1987`, `1988`, `1989`, 
