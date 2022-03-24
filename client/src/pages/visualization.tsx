c269d89955fc691b08c4f3f6d82ef39aa479403d
ch-ant <ant.chr@outlook.com>
2022-03-24 13:53:09 +0200

    Modify LineChartComponent
    - Refactor LineChartComponent to FunctionComponent
    - LineChartComponent can now (dynamically) accept data as a property
    - Move dummy data to visualization page (the query result data should be here)

--- client/src/pages/visualization.tsx
+++ client/src/pages/visualization.tsx
@@ -6,6 +6,53 @@ import LoadingComponent from '../components/Loading';
 import logging from '../config/logging';
 import IPageProps from '../interfaces/page';
 
+// Dummy data for demo
+// Should be deleted at some point
+const data = [
+    {
+        name: 'A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400
+    },
+    {
+        name: 'B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210
+    },
+    {
+        name: 'C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290
+    },
+    {
+        name: 'D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000
+    },
+    {
+        name: 'E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181
+    },
+    {
+        name: 'F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500
+    },
+    {
+        name: 'G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100
+    }
+];
+
 const Visualization: React.FunctionComponent<IPageProps> = (props) => {
     const [loading, setLoading] = useState<boolean>(true);
 
@@ -25,7 +72,7 @@ const Visualization: React.FunctionComponent<IPageProps> = (props) => {
             <CenterPiece>
                 <Card className="text-center p-5">
                     <CardBody>
-                        <LineChartComponent />
+                        <LineChartComponent data={data} />
                     </CardBody>
                 </Card>
             </CenterPiece>
